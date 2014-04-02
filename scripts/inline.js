7421f9196f81a83c4c6a8ea24148c8d379878c30
Brian Ford <btford@umich.edu>
2014-04-01 17:49:54 -0700

    chore: fix spacing output of inline script

--- scripts/inline.js
+++ scripts/inline.js
@@ -2,13 +2,14 @@
 // This is a really dumb naive approach that only supports `module.exports =`
 
 var fs = require('fs');
-var r = new RegExp("( *).*?require\\('(.+?)'\\)", 'g');
+var r = new RegExp("( *)(.*?)require\\('(.+?)'\\)", 'g');
 
 module.exports = function () {
   var debug = fs.readFileSync(__dirname + '/../content-scripts/inject.js', 'utf8');
 
-  var out = debug.replace(r, function (match, before, file) {
-    return ex(file, before);
+  var out = debug.replace(r, function (match, whitespace, before, file) {
+    return whitespace + '// exported from ' + file + '\n' +
+           whitespace + before + ex(whitespace, file);
   });
 
   fs.writeFileSync(__dirname + '/../content-scripts/inject.build.js', out);
@@ -16,19 +17,18 @@ module.exports = function () {
 
 // takes the contents of a file, wraps it in a closure
 // and returns the result
-function ex (file, whitespace) {
+function ex (whitespace, file) {
   var contents = fs.readFileSync(__dirname + '/../content-scripts/' + file, 'utf8');
   contents = contents.replace('module.exports = ', 'return ');
-  contents = ['// exported from ' + file,
-              '(function () {'].
+  contents = ['(function () {'].
               concat(contents.split('\n').
-              map(function (line) {
-                return '  ' + line;
-              })).
+                map(function (line) {
+                  return '  ' + line;
+                }).
               concat(['}())']).
               map(function (line) {
                 return whitespace + line;
-              }).join('\n');
+              })).join('\n');
 
   return contents;
 }
