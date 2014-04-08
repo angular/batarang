514eddcda9f48336e3d069c89b2c287e177e6e6b
Brian Ford <btford@umich.edu>
2014-04-08 14:27:45 -0700

    chore: clean up instrumentation test config

--- karma-inject.conf.js
+++ karma-inject.conf.js
@@ -1,16 +1,12 @@
-// Karma configuration for testing injected
-// AngularJS instrumentation
+// Karma configuration for testing injected AngularJS instrumentation
 
 module.exports = function (config) {
   config.set({
 
-    // base path, that will be used to resolve files and exclude
     basePath: '',
 
-    // frameworks to use
     frameworks: ['jasmine'],
 
-    // list of files / patterns to load in the browser
     files: [
       'test/inject/mock/*.js',
       'content-scripts/inject.build.js',
@@ -19,43 +15,31 @@ module.exports = function (config) {
       'test/inject/*.js'
     ],
 
-    // list of files to exclude
+    preprocessors: {
+      'content-scripts/inject.build.js': ['coverage']
+    },
+
     exclude: [
       '*.min.js'
     ],
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
-    // web server port
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
+
     port: 9876,
 
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
     browsers: ['Chrome'],
 
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
-
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
     singleRun: false
   });
 };
