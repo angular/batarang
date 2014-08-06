f3703f423f37678c5a4e19d8edf26e5b10e77922
Carlo s A. Guillen <caguillen@google.com>
2014-08-06 14:46:25 -0700

    feat(*): set up UI

--- inject.js
+++ inject.js
@@ -1,9 +1,9 @@
 
 // inject into the application context from the content script context
 
-
 var script = window.document.createElement('script');
 script.src = chrome.extension.getURL('hint.bundle.js');
+
 var html = document.getElementsByTagName('html')[0];
 html.setAttribute('ng-hint', '');
 
