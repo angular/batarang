d0da55123f350403efa2444a6dc6aad9ee741ec4
Erin Altenhof-Long <ealtenhof@google.com>
2014-08-08 11:39:18 -0700

    feat(buffer): buffer messages before devTools open
    Before the devTools are opened, messages should be buffered. This way they can
    be delivered when the devTools are opened.

--- background.js
+++ background.js
@@ -1,6 +1,13 @@
-
+var buffer = [];
+function addToBuffer(message) {
+  buffer.push(message);
+}
+chrome.runtime.onMessage.addListener(addToBuffer);
 chrome.runtime.onConnect.addListener(function(port) {
-
+  chrome.runtime.onMessage.removeListener(addToBuffer);
+  buffer.forEach(function(msg) {
+    port.postMessage(msg);
+  });
   // context script –> background
   chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     port.postMessage(msg);
