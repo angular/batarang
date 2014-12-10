2d92189c480954f196795e8a3d36a7beff0bf2fa
Brian Ford <btford@umich.edu>
2014-12-09 17:42:36 -0800

    chore(gulp): add tasks to help automate releases

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
+var zip = require('gulp-zip');
 
 var main = require('./package.json').main;
+// TODO: make sure manifest version === package.json version === bower.json version
+var version = require('./manifest.json').version;
 
 gulp.task('watch', function(){
   gulp.watch(['hint.js', '!./dist/*.js'], ['browserify']);
@@ -11,6 +14,29 @@ gulp.task('watch', function(){
 gulp.task('browserify', function() {
   var bundleStream = browserify('./' + main).bundle().pipe(source(main));
   return bundleStream.pipe(gulp.dest('./dist'));
+});
+
+/*
+ * I use this to make a zip for the chrome store
+ */
+gulp.task('package', ['browserify'], function () {
+  return gulp.src([
+      './dist/**',
+      './img/**',
+      './panel/**',
+      'background.js',
+      'devtoolsBackground.*',
+      'inject.js',
+      'manifest.json',
+      './bower_components/angular/angular.js'
+    ], {base: '.'})
+    .pipe(gulp.dest('./package'));
+});
+
+gulp.task('zip', ['package'], function () {
+  return gulp.src('package/**')
+      .pipe(zip('batarang-' + version + '.zip'))
+      .pipe(gulp.dest('.'));
 });
 
 gulp.task('default', ['browserify']);
