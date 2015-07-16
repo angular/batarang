ac18669209f38a5f15146e1c001332bdf85e9744
gary-b <garybarnett101@gmail.com>
2015-07-17 00:19:24 +0100

    fix(devtoolsBackground) ElementPanel and tab display
    Content was not loading in the AngularJS tab and the ElementPanel
    display showed an error message. Fixing URL location for AngularJS
    tab and ensuring all referenced objects are within scope of code that
    drives ElementPanel.
    
    Fixes #242

--- devtoolsBackground.js
+++ devtoolsBackground.js
@@ -1,19 +1,18 @@
 var panels = chrome && chrome.devtools && chrome.devtools.panels;
 
-function getScope(node) {
-  var scope = window.angular.element(node).scope();
-  if (!scope) {
-    // Might be a child of a DocumentFragment...
-    while (node && node.nodeType === 1) node = node.parentNode;
-    if (node && node.nodeType === 11) node = (node.parentNode || node.host);
-    return getScope(node);
-  }
-  return scope;
-}
-
 // The function below is executed in the context of the inspected page.
 
 var getPanelContents = function () {
+  function getScope(node) {
+    var scope = window.angular.element(node).scope();
+    if (!scope) {
+      // Might be a child of a DocumentFragment...
+      while (node && node.nodeType === 1) node = node.parentNode;
+      if (node && node.nodeType === 11) node = (node.parentNode || node.host);
+      return getScope(node);
+    }
+    return scope;
+  }
   if (window.angular && $0) {
     //TODO: can we move this scope export into updateElementProperties
     var scope = getScope($0);
@@ -52,7 +51,7 @@ panels && panels.elements.createSidebarPane(
   var angularPanel = panels.create(
     "AngularJS",
     "img/angular.png",
-    "panel.html"
+    "panel/app.html"
   );
 });
 
