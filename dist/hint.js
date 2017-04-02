a8336d4d8e3b5e0048ad69bd402caf11c2405d5a
Georgios Kalpakas <kalpakas.g@gmail.com>
2017-04-02 15:45:57 +0300

    fix(hint): do not crash
    Somewhere between v0.14.4 and v4.0.0, `EventEmitter2` started being inconsistent
    wrt to the arguments passed to the `onAny()` listener; sometimes the `eventType`
    is passed as the first argument, sometimes it isn't. This caused batarang to
    crash.
    
    This commit fixes it, by accounting for the fact that `eventType` might or might
    not be passed as a first argument.
    
    Fixes #310

--- dist/hint.js
+++ dist/hint.js
@@ -7,11 +7,19 @@
 require('./loader.js');
 require('angular-hint');
 
-angular.hint.onAny(function (data, severity) {
+angular.hint.onAny(function (event, data, severity) {
+  // EventEmitter2 usually omits the event type for the argument list (assigning it to `this.event`
+  // instead), but under certain circumstances it may include it.
+  if (this.event !== event) {
+    severity = data;
+    data = event;
+    event = this.event;
+  }
+
   window.postMessage({
     __fromBatarang: true,
-    module: this.event.split(':')[0],
-    event: this.event,
+    module: event.split(':')[0],
+    event: event,
     data: data,
     severity: severity
   }, '*');
